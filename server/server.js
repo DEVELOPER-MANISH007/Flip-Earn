bae298d030f63d80680e17e22c7d80531c131622
MANISH KUMAR <manish8755026341@gmail.com>
2025-12-02 23:31:52 +0530

    fix db.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import "dotenv/config"
 import cors from 'cors'
-import connectDB from './config/db.js'
+
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./Inngest/index.js"
 
 const app = express()
 
-connectDB()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 app.use(cors())
